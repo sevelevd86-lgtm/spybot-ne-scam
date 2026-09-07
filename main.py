c013735c6008b0f0c1c707a7ca6bb8ecf53978df
sevelevd86-lgtm <sevelevd86@gmail.com>
2026-09-07 23:19:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from aiogram.types import Message
 # ============================================================
 
 # ВСТАВЬ СЮДА ТОКЕН БОТА ИЗ BOTFATHER
-BOT_TOKEN = "ВСТАВЬ_СЮДА_ТОКЕН_БОТА"
+BOT_TOKEN = "8893376358:AAHVWJwm8GLJjqz_BWZiFV3CAsquDGsf44c"
 
 DATABASE_FILE = "business_monitor.db"
 

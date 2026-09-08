4ce689d85f0595aefb80c9e9a9fd9ca140eefd04
sevelevd86-lgtm <sevelevd86@gmail.com>
2026-09-08 17:59:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ from aiogram.utils.keyboard import (
 # НАСТРОЙКИ
 # ============================================================
 
-BOT_TOKEN = ""
+BOT_TOKEN = "8893376358:AAHVWJwm8GLJjqz_BWZiFV3CAsquDGsf44c"
 
 DB_PATH = "business_monitor.db"
 

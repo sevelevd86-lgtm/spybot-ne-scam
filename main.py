9c6dab427efc1ee346d14c63ecd55ca74f17a050
sevelevd86-lgtm <sevelevd86@gmail.com>
2026-09-14 22:16:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from aiogram.types import (
 # CONFIG
 # ============================================================
 
-BOT_TOKEN = ""
+BOT_TOKEN = "8893376358:AAHVWJwm8GLJjqz_BWZiFV3CAsquDGsf44c"
 
 DB_NAME = "business_monitor.db"
 
